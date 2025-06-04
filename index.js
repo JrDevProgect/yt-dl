d2a075c37431ac3238dff26e3a65b0f8b396c938
JrDevProgect <jrbusaco2@gmail.com>
2025-06-04 00:53:25 +0000

    t

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const searchRouter = require('./routes/search');
 const downloadRouter = require('./routes/download');
 const suggestionsRouter = require('./routes/suggestions');
@@ -9,14 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/search', searchRouter);
 app.use('/api/download', downloadRouter);
 app.use('/api/suggestions', suggestionsRouter);
 
-app.get('/', (req, res) => {
-  res.send('Server is running. Use the API endpoints to interact with the application.');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
