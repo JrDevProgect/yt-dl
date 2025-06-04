e4a352682bc06e5302ec278d9317c029d15bf9ab
JrDevProgect <jrbusaco2@gmail.com>
2025-06-04 00:58:17 +0000

    t

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use('/api/search', searchRouter);
 app.use('/api/download', downloadRouter);
 app.use('/api/suggestions', suggestionsRouter);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
